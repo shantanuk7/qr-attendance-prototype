d7d39de442aca137f93196a2d057bfc3fa42f49a
Atharva Naik <atharvan.coder@gmail.com>
2023-10-26 05:40:36 +0530

    Student Scan

---
+++ src/app/api/students/attendance/route.ts
@@ -0,0 +1,49 @@
+//This api route is completely responsible for handling QR code scan processing
+// - First checks 
+
+import { connect } from "@/dbConfig/dbconfig";
+import { NextResponse, NextRequest } from "next/server";
+import Lecture from "@/model/lectureModel";
+import mongoose from "mongoose";
+
+connect();
+
+export async function POST(request: NextRequest) {
+    console.log("Got the request on api route. Now starting with the request processing...");
+
+    const req = await request.json();
+    console.log("Request: " + req.data);
+    const { studEmail, lectureid, qrcodetext } = req;
+
+    console.log("Checking if the lecture for requested qr code exists...");
+    try {
+        console.log("Lecture id is : ", lectureid);
+
+        const lectureIdAsObjectId = new mongoose.Types.ObjectId(lectureid);
+
+        try {
+            // Use the static findByIdAndUpdate method on the Mongoose model
+            const updatedLecture = await Lecture.findByIdAndUpdate(
+                lectureIdAsObjectId,
+                { $addToSet: { attendees: [studEmail] } },
+                { new: true }
+            );
+
+            if (updatedLecture) {
+                console.log("Attendance recorded:", updatedLecture);
+                console.log("Done. QR code was correct and attendance has been marked.");
+                return NextResponse.json({success:true, message:"Done. QR code was correct and attendance has been marked."})
+            } else {
+                console.log("Lecture not found.");
+                return NextResponse.json({success:false, message:"Lecture with that QR was not found."})
+            }
+        } catch (error) {
+            console.error("Error updating lecture:", error);
+            return NextResponse.json({success:false, message:"Error updating lecture"})
+        }
+        
+    } catch (error) {
+        console.log("Error while checking lecture existence: ", error);
+        return NextResponse.json({success:false, error:error})
+    }
+}
--- src/app/student/ui-components/scan/page.tsx
+++ src/app/student/ui-components/scan/page.tsx
@@ -1,43 +1,104 @@
-"use client";
+"use client"
 import { Html5QrcodeScanner } from "html5-qrcode";
-import { useState,useEffect } from "react";
-import Link from "next/link";
-const QRReaderComponent = ()=>{
-  const [scanerRsult,setScanerResult] = useState(null);
-
-  useEffect(()=>{
-    const scan =new Html5QrcodeScanner('render',{
-      qrbox:{
-        width:250,
-        height:250,
-      },
-      fps:5,
-    },true);
-    scan.render(success,error)
-    function success(result:any){
-      scan.clear();
-      setScanerResult(result);
-    }
-    function error(error:any){
-     console.warn(error);
-    }
-  
-  },[])
-
-console.log(Html5QrcodeScanner)
-  return (
-    <div >
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Toaster, toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+export default function Home() {
+  const router = useRouter()
+  const [scanResult, setScanResult] = useState(null);
+  const [attendance, setAttendance] = useState(false);
+  const [email, setEmail] = useState<string | null>(null);
+  useEffect(() => {
+    const checkUser = async () => {
+      const getUserDetails = async () => {
+        try {
+          const res = await axios.get('/api/users/me/student');
+          setEmail(res.data.data.email);
+        } catch (error) {
+          console.error('Error fetching user details:', error);
+        }
+      };
     
-      
-     
-    <h1>QR code Scan</h1>
-    <div >
-      {scanerRsult?<div>Success:<Link href={'http://'+scanerRsult}>
-        {scanerRsult}
-        </Link></div>:<div id="render"></div>}
-    </div>
-   </div>
-  );
-};
+      // Call the function to fetch user details when the component mounts.
+      getUserDetails();
+    const initializeScanner = async () => {
+      const scanner = new Html5QrcodeScanner('reader', {
+        qrbox: {
+          width: 250,
+          height: 250,
+        },
+        fps: 3,
+      },
+      false //verbose
+      );
 
-export default QRReaderComponent;
+      const onScanSuccess = async (decodedText:any, decodedResult:any) => {
+        try {
+          console.log(`Code matched = ${decodedText}`, decodedResult);
+          scanner.clear();
+          setScanResult(decodedText);
+
+          const dataInJson = JSON.parse(decodedText);
+          console.log("this data is being sent to validate: ", dataInJson);
+          const { lectureid, qrcodetext } = dataInJson;
+
+          const sendData = {
+            studEmail: email,
+            lectureid: lectureid,
+            qrcodetext: qrcodetext,
+          };
+
+          console.log("Starting axios post request...");
+          const req = await axios.post("/api/students/attendance", sendData);
+          console.log(req.data);
+          setAttendance(req.data.success);
+          toast.success('Successfully Marked!')
+          router.push("/student")
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      const onScanFailure = (error:any) => {
+        console.warn(`Code scan error = ${error}`);
+      };
+
+      scanner.render(onScanSuccess, onScanFailure);
+    };
+
+    if (email) {
+      initializeScanner();
+    }
+  }
+  checkUser();
+},[email, router]);
+
+  return (
+    <main className="">
+      <Toaster
+  position="top-center"
+  reverseOrder={false}
+/>
+  {email ? (
+    <div>
+      <div id="reader"></div>
+    </div>
+  ) : (
+    <div>
+      {email ? (
+        scanResult && attendance ? (
+          <div className="text-center p-5">
+            Congratulations!! Your attendance has been marked!
+          </div>
+        ) : (
+          <div id="reader"></div>
+        )
+      ) : (
+        <div>Please login with a registered account</div>
+      )}
+    </div>
+  )}
+</main>
+  );
+}
